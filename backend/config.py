0d50df256743a23d6e3043644510963367c8dacd
Suraj Pawar <surajpawar0216@gmail.com>
2026-06-13 20:29:59 +0530

    Ready for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
+import os
 
 engine = create_engine(
-    "postgresql://postgres:0216@localhost/archive_system"
+    os.getenv("postgresql://neondb_owner:npg_BVZC0HSRgJF3@ep-silent-block-aoohiyq0-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 )
